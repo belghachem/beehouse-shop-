6cad5dfdd1eda5e40178827d1f91a301e9d71505
belghachem <149454757+belghachem@users.noreply.github.com>
2025-12-20 18:47:05 +0100

    Delete products directory

--- products/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
-
-def prodects_page(request):
-    return render(request, 'products/prodect_page.html')
