0228937b15dbc46c0ccea3286d4ca694b0254492
belghachem <149454757+belghachem@users.noreply.github.com>
2025-12-20 23:34:06 +0100

    Update settings.py

--- BeeHouse/settings.py
+++ BeeHouse/settings.py
@@ -124,11 +124,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Whitenoise configuration for static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # if you have a 'static' folder in your project root
-]
 # MEDIA files (userh uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
